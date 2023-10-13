400f365971c0f6a9e14c25a0da954541ee77d6e4
ABISHEK V <110534902+abishek70@users.noreply.github.com>
2023-10-13 15:18:51 +0530

    Add files via upload

---
+++ Day backend 5/frontend/src/App.js
@@ -0,0 +1,37 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import './Assets/CSS/App.css';
+import Sign from './component/Sign';
+import Home from './component/Home';
+import Login from './component/Login';
+import Profile from './component/Profile';
+import Recharge from './component/Recharge';
+import Offer from './component/Offer';
+import Faqjsx from './component/Faqjsx';
+import { Successful } from './component/successful';
+import Privacyjsx from './component/Privacyjsx';
+import About from './component/About';
+import TermsAndConditions from './component/Termsandconditions';
+import MobileRechargeCreate from './component/MobileRechargeCreate';
+import axios from 'axios';
+
+function App() {
+  return (
+    <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Login/>}/>
+          <Route path='/signup' element={<Sign/>}/>
+          <Route path='/Home' element={<Home/>}/>
+          <Route path='/Profile' element={<Profile/>}></Route>
+          <Route path='/Recharge' element={<MobileRechargeCreate/>}></Route>
+          <Route path='/Offer' element={<Offer/>}></Route>
+          <Route path='/faq' element={<Faqjsx/>}></Route>
+          <Route path='/successful' element={<Successful/>}></Route>
+          <Route path='/Privacy' element={<Privacyjsx/>}></Route>
+          <Route path='/About' element={<About/>}></Route>
+          <Route path='/Terms' element={<TermsAndConditions/>}></Route>
+        </Routes>
+        </BrowserRouter>
+  );
+}
+
+export default App;
---
+++ Day backend 5/frontend/src/component/Privacyjsx.jsx
@@ -0,0 +1,114 @@
+import React from 'react';
+import Privacy from '../Assets/CSS/Privacy.css'
+const Privacyjsx = () => {
+  return (
+    <div>
+    <div className="privacy">
+      <h1>Terms and Conditions</h1>
+      <p className='styletext'>
+        
+        
+      This is an introductory offer for our customers to experience high speed 5G data.
+      <br />
+      <br></br>
+Customers who have 5G-enabled devices and have latched onto the Airtel 5G Plus network can now enjoy Unlimited 5G Data.
+Customers need to claim the Unlimited 5G Data offer from Airtel Thanks App.
+<br></br>
+<br></br>
+All Airtel Prepaid customers who recharge with Unlimited packs starting from Rs239 and above are eligible to claim and experience unlimited 5G Data. To enjoy continued benefits, customers need to recharge with eligible pack. Please note prepaid packs Rs455 and Rs1799 are not eligible for getting Unlimited 5G Data benefits.
+This offer is also available for all Postpaid customers and can be claimed on Airtel Thanks App
+<br></br>
+<br></br>
+Unlimited 5G Data is over and above the existing Data quota in respective plans for both prepaid and postpaid.
+It is clarified and stated that these offers are only meant for personal and non-commercial use only. Airtel reserves the right to end / suspend / modify the Offer / services in specific cases of commercial use, or any fraudulent use.
+Sharing of Data where the customer has activated the Unlimited 5G Data shall not be allowed via mobile hotspots.
+<br></br>
+Airtel 5G plus is available in selected areas, depending on the network availability unlimited 5G data can only be used in 5G Network areas.
+<br></br>
+<br></br>
+These terms of use ("Agreement") for the Xstream AirFiber Services are a legal and binding agreement between ("you", "your" or "yourself"), as the User and Bharti Airtel Limited and/or its affiliates ("we", "us", "our", “Bharti Airtel Limited" or “Airtel”). This Agreement governs your use of the Xstream AirFiber Services. Please read this Agreement carefully before using the Xstream AirFiber Services.
+<br></br>
+<br></br>
+ 
+2. This Agreement applies to your use of any of our products or services including but not limited to hardware products, including without limitation, (i) Airtel Airfiber Devices, (referred to herein as the “Airtel Hardware") and/or (ii) the Airtel application, technical support and services and all associated with the Airtel Hardware.
+<br></br>
+<br></br>
+3. Your use of the Airtel Hardware is subject to the warranty terms and conditions as detailed at Warranty page, which is incorporated herein by reference.
+<br></br>
+<br></br>
+4. Service availability: Fixed wireless access service may not be available in all areas or at the rates, speeds, or bandwidth generally marketed, and some locations may not qualify for the Services even if initial testing showed that your location was qualified/ eligible. Service availability is dependent on coverage area and signal strength.
+<br></br>
+<br></br>
+5. Speed and bandwidth: Airtel may offer different speeds and bandwidth limits depending on the plan you choose. The bandwidth available to each device connected to your Home Network will vary depending upon the number, type, age, software operating system, and configuration of devices using the Services and the type of use (for example, streaming media), among other factors.. The speed of the Services may vary based on network or Internet congestion, your device configuration, your use and consumption of streaming or other digital media services, the condition of facilities used to deliver the Services and the wiring inside your location, among other factors.
+<br></br>
+<br></br>
+6. Xstream AirFiber Plan
+<br></br>
+<br></br>
+We offer a plan of INR 799 per month upon payment of six (6) months advance fee.
+<br></br>
+<br></br>
+The plan offers unlimited 5G data with speed upto 100 Mbps subject to fair usage policy of 3333 GB per month along with subscription to Xstream Premium Pack. Post the exhaustion of the data quota, you will get a throttled speed of 2mbpsThe plan only offers data without any voice calling or SMS facility.
+<br></br>
+<br></br>
+Additionally, we may charge an equipment fee, security deposit as well as a fee for installation and setup.
+<br></br>
+<br></br>
+7. Equipment and installation: Xstream AirFiber typically require installation of specialized equipment, such as a receiver or antenna especially in case of outdoor units. You require the Airtel-supplied Equipment fitted with an Airtel SIM to use this service. The SIM supplied with the modem must not be removed from the modem and cannot be used with any other device. The Airtel-supplied equipment remains the property of Airtel. You must return the Airtel-supplied modem, devices or any other equipment in good working order within 30 days of cancellation, failing which Airtel shall be entitled to forfeit the security deposit paid by you.
+<br></br>
+<br></br>
+8. Xstream AirFiber and associated Airtel Hardware (together “Customer Premise Equipment’s (CPE)”) shall be provided on “As-It-Is-Basis” and shall not tamper with or in any manner, including but not limited to unlocking or jail-breaking of the Airtel CPE.
+<br></br>
+<br></br>
+Nothing contained herein shall be construed as creating any arrangement for transfer of title, ownership or interest including rights of any intellectual property or CPE in favor of the You/Subscriber.
+<br></br>
+<br></br>
+You agree not to sell, lease, assign, and transfer or otherwise dispose of the Airtel CPE in any manner whatsoever.
+<br></br>
+<br></br>
+You agree to pay additional amount as determined by Airtel for replacement of CPE, if such replacement takes place one year after the installation.
+<br></br><br></br>
+ 
+The Airtel CPE shall at all times remain the property of Airtel and you must return the CPE at the time of disconnecting your postpaid subscriprtion.
+<br></br>
+<br></br>
+Airtel reserves the right to repossess the CPE if you fail to pay the subscription fee for continuous period of 1 month.
+<br></br>
+<br></br>
+9. Relocating to a new address: Before relocating to a new address, you must contact us to perform a serviceability check to determine if the AirFiber service is available at your new address. If your new address is not 5G serviceable, we will offer you an alternative service subject to availability. Airtel will not responsible if the service is not available in your new location if not informed. In the event Airtel repossesses the CPE in terms of this clause, Authorized representative shall visit your address for uninstallation CPE. You agree to provide necessary access of your premises to Airtel’s authorizes representatives to carry out the uninstallation process.
+<br></br>
+<br></br>
+10. Service fees: You agree to pay all applicable Charges for Services and Equipment by the due date set forth in your monthly invoice. Charges may be changed at any time and shall be in line with TRAI regulations.
+<br></br>
+<br></br>
+11. Security: Airtel reserves the right to modify settings on any Airtel managed access points (for example, routers and extenders) on your Home Network for safety and security reasons.
+<br></br>
+You agree that we and our third-party suppliers are not responsible for the loss of your or any third-party data or for the back-up or restoration of your or any third-party data regardless of whether this data is maintained on our servers or yours, or on your or your permitted guests’ device(s). You should always back-up any important information separately from data stored on our or any third party's servers.
+You agree that you are solely responsible for maintaining the security of Other Devices and data, including without limitation, encryption of data and protection of your User ID, user password, Home Network and administrative passwords, and personal and other data. The Services may permit you with the option to provide third parties authorized by you access to the Internet using your device.
+Use of our network for the distribution, operation, and maintenance of malicious software (viruses, bots, worms, Trojan horses, etc.) is prohibited.
+The CPE provided may include security software and other features that may be activated at your option. You are solely responsible for the activation and use of the optional features and settings.
+You agree to follow all rules and policies applicable to the Services, including required or automated updates, modifications and/or reinstallations of Access Software or other software and obtaining available patches to address security, interoperability and/or performance issues. You also agree: (i) to comply with all applicable laws, rules and regulations when using the Services and accessing or viewing Content; and (ii) not to reverse engineer, decompile, disassemble or otherwise tamper with or modify any of the security features, usage restrictions or other Services components for any reason (or help anyone else to do so).
+<br></br>
+<br></br>
+12. Termination: You or Airtel may terminate the Services at any time and for any reason by giving notice in accordance with the notice provisions set forth in this Agreement. Unless otherwise required by applicable law, termination will be effective on the last day of that month’s billing cycle, and you are responsible for all charges incurred through the date of termination. Installation or setup fees paid at the initiation of the Services, if any, are not refundable. In the event Airtel determines fraud, abuse or other violations of law or this Agreement, we may terminate your Service(s) immediately with no Notice.
+<br></br>
+<br></br>
+ 
+13. Acceptable use policy:
+<br></br>
+Legal Authority: You acknowledge that you are eighteen (18) years of age or older, and that you have the legal authority to enter into this Agreement.
+Premises Access: You agree to allow us and our representatives the right, at reasonable times, to enter the Premises for the purpose of installing, maintaining, servicing, inspecting, upgrading and/or removing the Services and any Equipment. Entry is subject to applicable law. We will make reasonable efforts to accommodate building access and homeowner or condominium association policies, but you agree to be responsible for violations of these policies related to access to the Premises.
+Right to Access: You acknowledge and agree that you are either an owner of, or legal tenant in, your Premises, and as such have the right to allow us and our representatives to enter and make modifications to the Premises in connection with the delivery of the Services.
+<br></br>
+<br></br>
+14. Governing Law: Any dispute or claim (contractual or non-contractual) arising out of or in relation to this Agreement, including disputes as to its formation, will be governed by and construed in accordance with Indian laws, without regard to its conflict of laws rules. Airtel and you submit to the exclusive jurisdiction of New Delhi courts.
+<br></br>
+It's important to carefully read and understand the terms and conditions of Xstream AirFiber before signing up. If you have any questions or concerns, be sure to reach out to us for further clarification.
+      </p>
+      {/* Add more content here */}
+    </div>
+    </div>
+  );
+}
+
+export default Privacyjsx
